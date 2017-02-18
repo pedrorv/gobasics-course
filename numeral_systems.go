3df182a1140921a124fc7155d618e622d1be3a7a
Pedro Reis <pedroreisv@gmail.com>
2017-02-18 17:17:05 -0200

    Printing numeral systems

---
+++ numeral_systems.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Printing a decimal number
+	fmt.Println(42)
+
+	// Printing a decimal number and it's binary representation
+	fmt.Printf("%d - %b \n", 42, 42)
+
+	// Printing a decimal number and it's octal representation
+	fmt.Printf("%d - %o \n", 42, 42)
+
+	// Printing a decimal number and it's hexadecimal representation
+	fmt.Printf("%d - %X \n", 42, 42)
+}
