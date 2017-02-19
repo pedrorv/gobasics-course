1713398aea6472da6c3dcaee123666b13ade0fac
Pedro Reis <pedroreisv@gmail.com>
2017-02-19 13:29:42 -0300

    Loops

---
+++ loops.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt" 
+
+func main() {
+
+  // Like a C for
+  for i := 0; i < 10; i++ {
+    fmt.Println(i)
+  }
+
+  j := 0
+  // Like a C while 
+  for j < 10 {
+    fmt.Println(j)
+    j++
+  }
+
+  k := 0
+  // No condition
+  for {
+    if k >= 20 {
+      break
+    }
+
+    fmt.Println(k)
+    k++
+  }
+
+}
