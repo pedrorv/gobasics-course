b53a545f716709fe39e8132476587c6594c4d2d5
Pedro Reis <pedroreisv@gmail.com>
2017-02-23 22:06:27 -0300

    composition

---
+++ composition.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Person struct {
+  first string
+  last string
+  age int
+}
+
+func (p Person) fullName() string {
+  return p.first + " " + p.last
+}
+
+func main() {
+  p1 := Person{"John", "Doe", 42}
+  fmt.Println(p1.fullName())
+}
