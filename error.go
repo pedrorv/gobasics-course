5de5a7e87d05593691d2fd9a1618455ffd0e6ff0
Pedro Reis <pedroreisv@gmail.com>
2017-03-04 13:11:02 -0300

    other error options

--- error.go
+++ error.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 )
 
+// import "log" package
+
 func main() {
 	_, err := os.Open("no-file.txt")
 	if err != nil {
 		fmt.Println("err:", err)
+		// log.Println()
+		// log.Fatalln(err)
+		// panic(err)
 	}
 }
