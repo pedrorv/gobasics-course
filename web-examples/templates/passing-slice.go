f9e74dcb2765b5294e030152d30dd2fa788cb96c
Pedro Reis <pedroreisv@gmail.com>
2017-03-18 13:47:21 -0300

    templates: passing slice with variables

--- web-examples/templates/passing-slice.go
+++ web-examples/templates/passing-slice.go
@@ -9,13 +9,18 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("tpl-slice.gohtml"))
+	tpl = template.Must(template.ParseFiles("tpl-slice.gohtml", "tpl-slice2.gohtml"))
 }
 
 func main() {
 	office := []string{"Dwight", "Jim", "Michael", "Pam"}
 
 	err := tpl.Execute(os.Stdout, office)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl-slice2.gohtml", office)
 	if err != nil {
 		log.Fatalln(err)
 	}
